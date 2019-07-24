1d89af745b3154118f261432b06189e5f2a465e4
greenc214 <greenc214@gmail.com>
2019-07-24 18:13:17 +0300

    updates

--- final-project/js/cart.js
+++ final-project/js/cart.js
@@ -101,7 +101,7 @@ function displayCart() {
                 <td class = "name"><a href = "details.html?id=${key}">${cart[key].name}</a></td>
                 <td class = "price" id = "${key}">$${cart[key].price}</td>
                 <td>${cart[key].stock}</td>
-                <td class = "quantityTd"><i data-id = "decrement${key}" class = "fas fa-arrow-circle-left decrement add-value my-auto" onclick = "reduce(${key});"></i><input type = "text" value = ${cart[key].quantity} data-id = "input${key}" class = "mx-1 quantity" disabled ><i data-id = "increment${key}" class = "fas fa-arrow-circle-right increment add-value my-auto" onclick = "increase(${key})"></i></td>
+                <td class = "quantityTd"><div class="my-auto"><i data-id = "decrement${key}" class = "fas fa-arrow-circle-left decrement my-auto" onclick = "reduce(${key});"></i><input type = "text" value = ${cart[key].quantity} data-id = "input${key}" class = "mx-1 quantity" disabled ><i data-id = "increment${key}" class = "fas fa-arrow-circle-right increment  my-auto" onclick = "increase(${key})"></i></div></td>
                 <td >$<span id = "total${key}" class = "total">${cart[key].price * cart[key].quantity}</span></td>
             </tr>
             `;
@@ -116,9 +116,8 @@ function displayCart() {
                         </tr>
                         <tr>
                             <th>Shipping</th>
-                            <td id="shipping+
-                            3">Free shipping<br>
-                                Shipping options will be updated during checkout.
+                            <td class="shipping">Free shipping<br>
+                                Shipping options will be updated during checkout
                             </td>
                         <tr>
                             <th>Total</th>
